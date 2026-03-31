b18af0cabdf971d87e3781bd59b56eab94c187a5
Sergey Malyuga <sergey@malyuga.ru>
2026-03-31 15:50:28 +0300

    feat(offer.reducer): добавить обработку actions
    - loadOffers: изменение состояния загрузки isLoading: true
    - loadOffersSuccess: обновление состояния в store, сброс ошибок и состояния загрузки
    - loadOffersFailure: сброс состояния загрузки, обновление ошибок
    
    Для управления состоянием загрузки офферов в NgRx store

--- src/app/core/models/offers.state.ts
+++ src/app/core/models/offers.state.ts
@@ -1,7 +1,8 @@
 import { OfferPreview } from './offers';
 import { EntityState } from '@ngrx/entity';
+import {HttpErrorResponse} from '@angular/common/http';
 
 export interface OffersState extends EntityState<OfferPreview> {
   isLoading: boolean;
-  error: string | null;
+  error: HttpErrorResponse | string | null;
 }
--- src/app/store/offer/offer.reducer.ts
+++ src/app/store/offer/offer.reducer.ts
@@ -1,12 +1,23 @@
-import { createEntityAdapter } from '@ngrx/entity';
-import { OfferPreview } from '../../core/models/offers';
-import { createReducer } from '@ngrx/store';
-import { OffersState } from '../../core/models/offers.state';
+import {createEntityAdapter} from '@ngrx/entity';
+import {OfferPreview} from '../../core/models/offers';
+import {createReducer, on} from '@ngrx/store';
+import {OffersState} from '../../core/models/offers.state';
+import {loadOffers, loadOffersFailure, loadOffersSuccess} from './actions/offer.actions';
 
 export const offerAdapter = createEntityAdapter<OfferPreview>();
-const initialState = offerAdapter.getInitialState({
+const initialState: OffersState = offerAdapter.getInitialState({
   isLoading: false,
   error: null,
 });
 
-export const offerReducer = createReducer<OffersState>(initialState);
+export const offerReducer = createReducer<OffersState>(initialState,
+  on(loadOffers, state => ({
+    ...state, isLoading: true,
+  })),
+  on(loadOffersSuccess, (state, {offers}) =>
+    offerAdapter.setAll(offers, {...state, isLoading: false, error: null}),
+  ),
+  on(loadOffersFailure, (state, {error}) => ({
+    ...state, isLoading: false, error
+  }))
+);
