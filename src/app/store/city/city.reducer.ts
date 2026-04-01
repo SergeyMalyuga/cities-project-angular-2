84d47e79d058dd75bfddc7845b63ca39a4ad6c24
Sergey Malyuga <sergey@malyuga.ru>
2026-04-01 15:51:46 +0300

    feat(store): создать состояние для city
    - Созданы reducer, actions, selectors
    - changeCity: action для смены текущего города
    - Инициализировано начальное состояние (initialState)

---
+++ src/app/store/city/city.reducer.ts
@@ -0,0 +1,12 @@
+import {City} from '../../core/models/city';
+import {DEFAULT_CITY} from '../../core/constants/const';
+import {createReducer, on} from '@ngrx/store';
+import {changeCity} from './actions/city.actions';
+
+const initialState: City = DEFAULT_CITY;
+
+export const cityReducer = createReducer(initialState,
+  on(changeCity, (state, {city}) => ({
+    ...state, city
+  }))
+);
