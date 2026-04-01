bc2d4bff256ff38df703dc2a55e310eabfd8c232
Sergey Malyuga <sergey@malyuga.ru>
2026-04-01 15:57:05 +0300

    feat(offer-card): создать компонент для карточки предложения
    - Добавлены: картинка гостиницы, цена, рейтинг, краткое описание
    - Интегрирован в список предложений в MainComponent

---
+++ src/app/store/app/selectors/app.selector.ts
@@ -0,0 +1,16 @@
+import {createFeatureSelector, createSelector} from '@ngrx/store';
+import {AppState} from '../../../core/models/app.state';
+import {offerAdapter} from '../../offer/offer.reducer';
+
+const selectOffersState = createFeatureSelector<AppState['offers']>('offers');
+const offersSelectors = offerAdapter.getSelectors();
+
+const selectCityState = createFeatureSelector<AppState['city']>('city');
+
+export const selectOffersByCity = createSelector(
+  selectOffersState,
+  selectCityState,
+  (offersState, cityState) =>
+    Object.values(offersState.entities).filter(offer => offer !== undefined)
+      .filter(offer => offer.city.name === cityState.name)
+);
