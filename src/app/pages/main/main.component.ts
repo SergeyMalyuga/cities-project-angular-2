e3b61fed82c488da5e1a3f4965362af83ae3136d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-02 15:59:46 +0300

    feat(main): добавить поле currentCity
    - Для реактивного выделения активного города в шаблоне

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,15 +2,20 @@ import {Component, inject} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
-import {selectOffersByCity} from '../../store/app/selectors/app.selector';
+import {selectCity, selectOffersByCity} from '../../store/app/selectors/app.selector';
 import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
+import {SelectCityDirective} from '../../shared/directives/select-city.directive';
+import {CITY_LOCATIONS} from '../../core/constants/const';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  imports: [HeaderComponent, OfferCardComponent],
+  imports: [HeaderComponent, OfferCardComponent, SelectCityDirective, NgClass],
 })
 export class MainComponent {
   private store = inject(Store<AppState>);
   public offers = this.store.selectSignal(selectOffersByCity);
+  public currentCity = this.store.selectSignal(selectCity);
+  protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
 }
