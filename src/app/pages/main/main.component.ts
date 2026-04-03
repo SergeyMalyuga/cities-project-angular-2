336759ba6b59d7dd68b1d189a782e7d1255faaf5
Sergey Malyuga <sergey@malyuga.ru>
2026-04-03 15:23:00 +0300

    feat(shared): создать директиву закрытия элемента при клике вне

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
@@ -7,15 +7,29 @@ import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.
 import {SelectCityDirective} from '../../shared/directives/select-city.directive';
 import {CITY_LOCATIONS} from '../../core/constants/const';
 import {NgClass} from '@angular/common';
+import {ToggleDirective} from '../../shared/directives/toggle.directive';
+import {ClickOutsideDirective} from '../../shared/directives/click-ouside.directive';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  imports: [HeaderComponent, OfferCardComponent, SelectCityDirective, NgClass],
+  imports: [HeaderComponent, OfferCardComponent, SelectCityDirective, NgClass, ToggleDirective, ClickOutsideDirective],
 })
 export class MainComponent {
   private store = inject(Store<AppState>);
+
   public offers = this.store.selectSignal(selectOffersByCity);
   public currentCity = this.store.selectSignal(selectCity);
+  public isOptionsOpen = signal<boolean>(false);
   protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
+
+  public toggleOptions() {
+    this.isOptionsOpen.set(!this.isOptionsOpen());
+  }
+
+  public closeOptions() {
+    if (this.isOptionsOpen()) {
+      this.isOptionsOpen.set(false);
+    }
+  }
 }
---
+++ src/app/shared/directives/click-ouside.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, ElementRef, EventEmitter, HostListener, inject, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appClickOutside]'
+})
+export class ClickOutsideDirective {
+  @Output() outsideClicked = new EventEmitter();
+
+  private elementRef = inject(ElementRef);
+
+  @HostListener('document:click', ['$event.target'])
+  onClick(target: HTMLElement) {
+    if (!this.elementRef.nativeElement.contains(target)) {
+      this.outsideClicked.emit();
+    }
+  }
+}
