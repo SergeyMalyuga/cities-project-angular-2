8383515aea0c03e9ecb0fe162235f2fc5cff49ff
Sergey Malyuga <sergey@malyuga.ru>
2026-04-06 10:06:00 +0300

    feat(main): добавить pipe для сортировки предложений
    - Сортирует предложения согласно текущему выбранному типу сортировки

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,24 +1,26 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {HeaderComponent} from '../../shared/components/header/header.component';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
 import {selectCity, selectOffersByCity} from '../../store/app/selectors/app.selector';
 import {OfferCardComponent} from '../../shared/components/offer-card/offer-card.component';
 import {SelectCityDirective} from '../../shared/directives/select-city.directive';
-import {CITY_LOCATIONS} from '../../core/constants/const';
+import {CITY_LOCATIONS, SortType} from '../../core/constants/const';
 import {NgClass} from '@angular/common';
 import {PlacesSortingComponent} from '../../features/places-sorting/places-sorting.component';
+import {SortOffersByPipe} from './pipes/sort-offers-by.pipe';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  imports: [HeaderComponent, OfferCardComponent, SelectCityDirective, NgClass, PlacesSortingComponent],
+  imports: [HeaderComponent, OfferCardComponent, SelectCityDirective, NgClass, PlacesSortingComponent, SortOffersByPipe],
 })
 export class MainComponent {
   private store = inject(Store<AppState>);
 
-  public offers = this.store.selectSignal(selectOffersByCity);
-  public currentCity = this.store.selectSignal(selectCity);
   protected readonly CITY_LOCATIONS = CITY_LOCATIONS;
 
+  public offers = this.store.selectSignal(selectOffersByCity);
+  public currentCity = this.store.selectSignal(selectCity);
+  public sortType = signal<SortType>(SortType.POPULAR);
 }
---
+++ src/app/pages/main/pipes/sort-offers-by.pipe.ts
@@ -0,0 +1,30 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {OfferPreview} from '../../../core/models/offers';
+import {SortType} from '../../../core/constants/const';
+
+@Pipe({
+  name: 'sortOffersBy'
+})
+export class SortOffersByPipe implements PipeTransform {
+  transform(offers: OfferPreview[] | undefined, sortType: SortType) {
+    if (!offers || !sortType) {
+      return [];
+    }
+
+    const sortOffers = [...offers];
+    switch (sortType) {
+      case SortType.PRICE_LOW_TO_HIGH: {
+        return sortOffers.sort((a, b) => a.price - b.price);
+      }
+      case SortType.PRICE_HIGH_TO_LOW: {
+        return sortOffers.sort((a, b) => b.price - a.price);
+      }
+      case SortType.TOP_RATED_FIRST: {
+        return sortOffers.sort((a, b) => b.rating - a.rating);
+      }
+      default: {
+        return sortOffers;
+      }
+    }
+  }
+}
