861c16bfd1c99cff03b3778f71098c3b1ae02c9d
Sergey Malyuga <sergey@malyuga.ru>
2026-04-06 11:09:21 +0300

    feat(places-sorting): подключить директиву выбора сортировки к списку опций

--- src/app/features/places-sorting/places-sorting.component.ts
+++ src/app/features/places-sorting/places-sorting.component.ts
@@ -1,20 +1,28 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output, signal} from '@angular/core';
 import {ToggleDirective} from '../../shared/directives/toggle.directive';
 import {ClickOutsideDirective} from '../../shared/directives/click-ouside.directive';
 import {NgClass} from '@angular/common';
+import {SortType} from '../../core/constants/const';
+import {SelectSortDirective} from './directives/select-sort-type.directive';
 
 @Component({
   selector: 'app-places-sorting',
   imports: [
     ToggleDirective,
     ClickOutsideDirective,
-    NgClass
+    NgClass,
+    SelectSortDirective
   ],
   templateUrl: './places-sorting.component.html',
   styleUrl: './places-sorting.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlacesSortingComponent {
+  @Input({required: true}) currentSortType!: SortType;
+  @Output() sortSelected = new EventEmitter<SortType>();
+  protected readonly Object = Object;
+  protected readonly SortType = SortType;
+
   public isOptionsOpen = signal<boolean>(false);
 
   public toggleOptions() {
@@ -25,5 +33,10 @@ export class PlacesSortingComponent {
     if (this.isOptionsOpen()) {
       this.isOptionsOpen.set(false);
     }
+  }
+
+  public onSortSelected(sortType: SortType) {
+    this.sortSelected.emit(sortType);
+    this.closeOptions();
   }
 }
