c42dd088a38355a94001bab7cfb588d67a5b095f
Sergey Malyuga <sergey@malyuga.ru>
2026-04-07 12:24:33 +0300

    feat(utils): создать обработчик http ошибок

---
+++ src/app/utils/handle-error.ts
@@ -0,0 +1,7 @@
+import {HttpErrorResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+
+export function handleError(error: HttpErrorResponse): Observable<never> {
+  console.error('Auth service error:', error);
+  return throwError(() => new Error('Authentication operation failed'));
+}
---
+++ src/app/utils/rxjs-operators.ts
@@ -0,0 +1,18 @@
+import {catchError, MonoTypeOperatorFunction, retry, throwError, timeout, timer} from 'rxjs';
+import {RETRY_ATTEMPTS, TIMEOUT_MS} from '../core/constants/const';
+import {handleError} from './handle-error';
+import {HttpErrorResponse} from '@angular/common/http';
+
+export function defaultHttpPipe<T>(): [MonoTypeOperatorFunction<T>, MonoTypeOperatorFunction<T>, MonoTypeOperatorFunction<T>] {
+  return [timeout(TIMEOUT_MS),
+    retry({
+      count: RETRY_ATTEMPTS,
+      delay: (error: HttpErrorResponse) => {
+        if (error.status === 0 || error.status >= 500) {
+          return timer(1500)
+        }
+        return throwError(() => error)
+      }
+    }),
+    catchError(handleError)]
+}
