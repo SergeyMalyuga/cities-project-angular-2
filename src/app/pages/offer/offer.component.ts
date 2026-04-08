c354b8c5a64706c823b4b23885c3af61b0ca8919
Sergey Malyuga <sergey@malyuga.ru>
2026-04-08 12:58:54 +0300

    feat(offer): реализовать загрузку оффера по id с сервера
    - Запрос выполняется при навигации на страницу оффера

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {Offer} from '../../core/models/offers';
+import {ActivatedRoute, Router} from '@angular/router';
+import {catchError, combineLatest, EMPTY, filter, map, switchMap} from 'rxjs';
+import {OfferService} from '../../core/services/offer.service';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-offer',
   templateUrl: './offer.component.html',
   imports: [HeaderComponent],
 })
-export class OfferComponent {}
+export class OfferComponent implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private offerService = inject(OfferService);
+  private destroyRef = inject(DestroyRef);
+
+  public offer = signal<Offer | null>(null);
+  public offerId = signal<string | null>(null);
+
+  public ngOnInit(): void {
+    this.activatedRoute.paramMap.pipe(map(params =>
+        params.get('id')),
+      filter((id): id is string => id !== null),
+      switchMap((id) => {
+        const offers$ = this.offerService.getOfferById(id).pipe(catchError(() => {
+          this.router.navigate(['/', '**']);
+          return EMPTY;
+        }));
+        return combineLatest({offer: offers$})
+      })).pipe(takeUntilDestroyed(this.destroyRef)).subscribe(result => {
+        this.offer.set(result.offer);
+        this.offerId.set(result.offer.id);
+      }
+    );
+  }
+}
