c0765838579118697dd2843abb83cd57db8158da
Sergey Malyuga <sergey@malyuga.ru>
2026-04-08 11:32:27 +0300

    feat(header): настроить навигацию на домашнюю страницу

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../../core/constants/const';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  imports: [
+    RouterLink
+  ]
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly AppRoute = AppRoute;
+}
