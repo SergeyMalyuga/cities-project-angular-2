4e608e193c83428c3b452fe5eb7fe388defe74d9
Sergey Malyuga <sergey@malyuga.ru>
2026-04-13 12:03:42 +0300

    feat(shared): создать директиву для обработки кликов на кнопках и CTA-ссылках

---
+++ src/app/shared/directives/accessibility-click.directive.ts
@@ -0,0 +1,21 @@
+import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appAccessibilityClick]',
+})
+export class AccessibilityClickDirective {
+  @Output() accessibilityClicked = new EventEmitter<void>();
+
+  @HostListener('click')
+  onClick() {
+    this.accessibilityClicked.emit()
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(evt: KeyboardEvent) {
+    if (evt.key === 'Enter' || evt.key === ' ') {
+      evt.preventDefault();
+      this.accessibilityClicked.emit();
+    }
+  }
+}
