f478f4c59b74ed80a41ed7306d52de76bf09fd9c
Sergey Malyuga <sergey@malyuga.ru>
2026-04-13 12:07:44 +0300

    feat(store): создать actions для обработки аутентификации пользователя

--- src/app/store/user/actions/user.actions.ts
+++ src/app/store/user/actions/user.actions.ts
@@ -1,8 +1,21 @@
 import {createAction, props} from '@ngrx/store';
 import {User} from '../../../core/models/user';
+import {Credentials} from '../../../core/models/credentials';
 
 export const checkAuth = createAction('[App Component] Check Auth');
 export const checkAuthSuccess = createAction('[User API] Check Auth Success',
   props<{ user: User }>());
 export const checkAuthFailure = createAction('[User API] Check Auth Failure',
+  props<{ error: string }>());
+
+export const login = createAction('[Login Component] Login',
+  props<{ credentials: Credentials }>());
+export const loginSuccess = createAction('p[User API] Login Success',
+  props<{ user: User }>());
+export const loginFailure = createAction('[User API] Login Failure',
+  props<{ error: string }>());
+
+export const logout = createAction('[Header Component] Logout');
+export const logoutSuccess = createAction('[User API] Logout Success');
+export const logoutFailure = createAction('[UserAPI] Logout Failure',
   props<{ error: string }>());
