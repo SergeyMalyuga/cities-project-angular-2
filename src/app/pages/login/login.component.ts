24fe725b81bf9132bee5daffe1db936284be711b
Sergey Malyuga <sergey@malyuga.ru>
2026-04-13 09:26:16 +0300

    feat(login): настроить переход на главную страницу
    - При клике на логотип в login пользователь переходит на главную страницу

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {AppRoute} from '../../core/constants/const';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [
+    RouterLink
+  ]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected readonly AppRoute = AppRoute;
+}
