0dfac7618e271dd5e727f77432097303e81195e8
Sergey Malyuga <sergey@malyuga.ru>
2026-04-14 10:49:36 +0300

    feat(reviews-form): добавить валидацию формы создания комментария

--- src/app/features/reviews-form/reviews-form.component.ts
+++ src/app/features/reviews-form/reviews-form.component.ts
@@ -1,11 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reviews-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './reviews-form.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReviewsFormComponent {
+  private formBuilder = inject(FormBuilder);
 
+  public reviewGroup: FormGroup = this.formBuilder.group({
+    comment: ['', [Validators.required, Validators.minLength(50)]],
+    rating: ['', [Validators.required]],
+  });
 }
