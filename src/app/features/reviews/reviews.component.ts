414538f79134db6bed640deb34d101168ad326b1
Sergey Malyuga <sergey@malyuga.ru>
2026-04-14 12:30:57 +0300

    feat(reviews): настроить отображение нового комментария

--- src/app/features/reviews/reviews.component.ts
+++ src/app/features/reviews/reviews.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, inject, Input, Output} from '@angular/core';
 import {Comment} from '../../core/models/comments';
 import {DatePipe} from '@angular/common';
 import {Store} from '@ngrx/store';
@@ -6,17 +6,21 @@ import {AppState} from '../../core/models/app.state';
 import {selectAuthStatus} from '../../store/user/selectors/user.selectors';
 import {AuthorizationStatus} from '../../core/constants/const';
 import {ReviewsFormComponent} from '../reviews-form/reviews-form.component';
+import {NewComment} from '../../core/models/new-comment';
+import {SortByDatePipe} from './pipes/sort-by-date.pipe';
 
 @Component({
   selector: 'app-reviews',
   imports: [
     DatePipe,
-    ReviewsFormComponent
+    ReviewsFormComponent,
+    SortByDatePipe
   ],
   templateUrl: './reviews.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReviewsComponent {
+  @Output() submitted = new EventEmitter<NewComment>();
   @Input({required: true}) comments!: Comment[];
 
   private store = inject(Store<AppState>);
@@ -24,4 +28,8 @@ export class ReviewsComponent {
   public readonly Math = Math;
   public authStatus = this.store.selectSignal(selectAuthStatus);
   protected readonly AuthorizationStatus = AuthorizationStatus;
+
+  public onSubmitted(comment: NewComment) {
+    this.submitted.emit(comment);
+  }
 }
