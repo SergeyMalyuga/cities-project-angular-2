2de45c0ee99502ed2937948ad0c69d87916c18cb
Sergey Malyuga <sergey@malyuga.ru>
2026-04-14 09:58:53 +0300

    feat(shared): добавить эмит события в директиве смены города

--- src/app/shared/directives/select-city.directive.ts
+++ src/app/shared/directives/select-city.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, HostListener, inject, Input} from '@angular/core';
+import {Directive, EventEmitter, HostListener, inject, Input, Output} from '@angular/core';
 import {City} from '../../core/models/city';
 import {Store} from '@ngrx/store';
 import {AppState} from '../../core/models/app.state';
@@ -9,12 +9,14 @@ import {changeCity} from '../../store/city/actions/city.actions';
 })
 export class SelectCityDirective {
   @Input({required: true}) city!: City;
+  @Output() citySelected = new EventEmitter<void>();
 
   private store = inject(Store<AppState>);
 
   @HostListener('click', ['$event'])
   onClick(evt: MouseEvent) {
     evt.preventDefault();
-    this.store.dispatch(changeCity({city: this.city}))
+    this.store.dispatch(changeCity({city: this.city}));
+    this.citySelected.emit();
   }
 }
