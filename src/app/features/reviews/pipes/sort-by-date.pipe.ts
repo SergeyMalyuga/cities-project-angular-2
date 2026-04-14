cb3bc3a746f73669c6c0cb32db2f9bdcb5f5cef7
Sergey Malyuga <sergey@malyuga.ru>
2026-04-14 12:26:34 +0300

    feature(reviews): добавтиь сортировку комментариев по дате

---
+++ src/app/features/reviews/pipes/sort-by-date.pipe.ts
@@ -0,0 +1,11 @@
+import {Pipe, PipeTransform} from '@angular/core';
+import {Comment} from '../../../core/models/comments';
+
+@Pipe({
+  name: 'sortByDate'
+})
+export class SortByDatePipe implements PipeTransform {
+  transform(comments: Comment[]) {
+    return [...comments].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  }
+}
