fc1ed606a72a0b864f543488f2793e04f04cedc8
Sergey Malyuga <sergey@malyuga.ru>
2026-04-15 12:24:29 +0300

    feat(favorite-offers): добавить свойство в модель корневого состояния
    - Расширен интерфейс AppState
    - Добавлено favoriteOffers: FavoriteOffersState

--- src/app/core/models/app.state.ts
+++ src/app/core/models/app.state.ts
@@ -1,11 +1,10 @@
 import {OffersState} from './offers.state';
 import {UserState} from './user.state';
-import {CityState} from './city.state';
 import {FavoriteOffersState} from './favorite-offers.state';
 import {City} from './city';
 
 export interface AppState {
-  /*  favoriteOffers: FavoriteOffersState;*/
+  favoriteOffers: FavoriteOffersState;
   city: City;
   offers: OffersState;
   user: UserState;
