f7d0d6e3cf2b6e3f70fbe8915b70524b3c4dce7f
Sergey Malyuga <sergey@malyuga.ru>
2026-04-15 10:30:17 +0300

    feat(service): создать сервис для работы с избранными офферами

---
+++ src/app/core/services/favorite-offers.service.ts
@@ -0,0 +1,23 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Offer, OfferPreview} from '../models/offers';
+import {APIRoute, BASE_URL, FavoriteStatus} from '../constants/const';
+import {defaultHttpPipe} from '../../utils/rxjs-operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoriteOffersService {
+  private http = inject(HttpClient);
+
+  public getOffers(): Observable<OfferPreview[]> {
+    return this.http.get<OfferPreview[]>(`${BASE_URL}/${APIRoute.FAVORITE}`).pipe(...defaultHttpPipe<OfferPreview[]>());
+  }
+
+  public toggleFavorite(offerId: string, isFavorite: boolean): Observable<Offer> {
+    const statusValue = isFavorite ? FavoriteStatus.ADDED : FavoriteStatus.REMOVED;
+    return this.http.post<Offer>(`${BASE_URL}/${APIRoute.FAVORITE}/${offerId}/${statusValue}`, {}).pipe(...defaultHttpPipe<Offer>());
+  }
+}
